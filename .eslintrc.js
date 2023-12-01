4800a031ac878c141f8dcf67b94c651c8f49690b
unicorndev727 <unicorndev727@gmail.com>
2023-12-01 03:31:23 -0600

    fix lint and format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,38 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
 	},
-	"extends": [
+	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
-		"plugin:react/recommended"
+		"plugin:react/recommended",
 	],
-	"overrides": [
+	overrides: [
 		{
-			"env": {
-				"node": true
+			env: {
+				node: true,
 			},
-			"files": [
-				".eslintrc.{js,cjs}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module"
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
 	},
-	"plugins": [
-		"@typescript-eslint",
-		"react"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"never"
-		],
-		"prettier/prettier": "error",
-	}
+	plugins: ["@typescript-eslint", "react"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "never"],
+		"react/react-in-jsx-scope": "off",
+		"react/jsx-uses-react": "off",
+		"react/no-unescaped-entities": 0,
+	},
 }
--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,11 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require("expo/metro-config")
+/* eslint-disable */
+const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-	// [Web-only]: Enables CSS support in Metro.
-	isCSSEnabled: true,
-})
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
 
-module.exports = config
+module.exports = config;
